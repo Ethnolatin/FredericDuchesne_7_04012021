0c061cf7f1e65db31cb877278d95d65f7b10cff1
Ethnolatin <duchesne.fred@gmail.com>
2021-01-05 18:12:20 +0100

    add authentication middleware for  articles routes (simplified token )

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,13 +1,15 @@
 /* eslint-disable no-throw-literal */
-const jwt = require('jsonwebtoken')
+import jwt from 'jsonwebtoken'
 
 // vérifie que l'utilisateur est authentifié
 module.exports = (req, res, next) => {
     try {
         // extrait le token du header 'authorization'
-        const token = req.headers.authorization.split(' ')[1]
+        // const token = req.headers.authorization.split(' ')[1]
+        const token =
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTYwOTg2NDM2MSwiZXhwIjoxNjA5OTUwNzYxfQ.30drwYSXPm4-48HBaJjHM215SvSyX1mavZbp3mAMiKE'
         // décode le token
-        const decodedToken = jwt.verify(token, process.env.TOKENKEY)
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET')
         const { userId } = decodedToken
         // vérifie le token
         if (req.body.userId && req.body.userId !== userId) {
--- src/routes/articlesRoutes.js
+++ src/routes/articlesRoutes.js
@@ -9,10 +9,10 @@ const router = Router()
 // - 'auth' renforce l'authentification sur les routes relatives aux articles
 // - 'multer' accepte les téléchargements de fichier pour 'create' et 'modify'
 // router.post('/:id/like', auth, articlesCtrl.likeArticle)
-router.post('/', articlesCtrl.createArticle) // remettre auth
-router.put('/:id', articlesCtrl.modifyArticle) // remettre auth
-router.delete('/:id', articlesCtrl.deleteArticle) // remettre auth
-router.get('/:id', articlesCtrl.getOneArticle) // remettre auth
-router.get('/', articlesCtrl.getAllArticles) // remettre auth
+router.post('/', auth, articlesCtrl.createArticle)
+router.put('/:id', auth, articlesCtrl.modifyArticle)
+router.delete('/:id', auth, articlesCtrl.deleteArticle)
+router.get('/:id', auth, articlesCtrl.getOneArticle)
+router.get('/', auth, articlesCtrl.getAllArticles)
 
 module.exports = router
