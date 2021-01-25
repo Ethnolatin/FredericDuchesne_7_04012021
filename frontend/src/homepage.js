a583ba4be81e2838e9546ca9ee7d623c64b935de
Ethnolatin <duchesne.fred@gmail.com>
2021-01-25 17:36:00 +0100

    user can like or dislike articles

--- frontend/src/homepage.js
+++ frontend/src/homepage.js
@@ -24,7 +24,8 @@ export class Homepage extends React.Component {
             showCreateModal: false,
             newArticleTitle: '',
             newArticleText: '',
-            articleModification: false
+            articleModification: false,
+            like: undefined
         }
 
 		this.getAllArticles = this.getAllArticles.bind(this)
@@ -79,7 +80,7 @@ export class Homepage extends React.Component {
         })
     }
 
-    modifyArticle = (event) => {
+    updateArticle = (event) => {
         event.preventDefault()
         const Id = this.state.Id
         const articleData = {
@@ -109,6 +110,20 @@ export class Homepage extends React.Component {
         })
     }
 
+    likeArticle = (selectedArticle, like) => {
+        const likeData = {userId: this.state.userId, like: like}
+        const Id = selectedArticle.Id
+        ajaxPost ('http://localhost:3000/api/articles/' + Id + '/like', likeData, this.context.token)
+        .then ((response) => {
+            console.log('thumb response: ', response)
+            this.getAllArticles()
+        })
+        .catch((err) => {
+            console.log({err})
+        })
+
+    }
+
     articleModalDisplay = (selectedArticle) => {
         this.setState({
             showArticleModal: true,
@@ -135,7 +150,7 @@ export class Homepage extends React.Component {
         })
     }
 
-    modify(selectedArticle) {
+    modifyArticle(selectedArticle) {
         this.setState({
             articleModification: true,
             Id: selectedArticle.Id,
@@ -161,6 +176,38 @@ export class Homepage extends React.Component {
         this.setState({[name]:value})
     }
 
+    handleThumbUpChange(selectedArticle, likeOption) {
+        console.log('likeOption: ', likeOption)
+        let like
+        switch (likeOption) {
+            case 3:
+                like = 1
+                break
+            case 2:
+                like = 0
+                break
+            default: return
+        }
+        console.log('like: ', like)
+        this.likeArticle(selectedArticle, like)
+    }
+
+    handleThumbDownChange(selectedArticle, likeOption) {
+        console.log('likeOption: ', likeOption)
+        let like
+        switch (likeOption) {
+            case 3:
+                like = -1
+                break
+            case 1:
+                like = 0
+                break
+            default: return
+        }
+        console.log('like: ', like)
+        this.likeArticle(selectedArticle, like)
+    }
+
     articlesList() {
         console.log('state: ', this.state)
         const articlesCollection = this.state.articlesCollection.reverse()
@@ -170,7 +217,7 @@ export class Homepage extends React.Component {
             lastWeek : 'dddd [dernier à] H[h]mm',
             sameElse : '[le] dddd D MMMM YYYY [à] H[h]mm'
         }
-		const publishArticle = this.state.articleModification ? this.modifyArticle : this.createArticle
+		const publishArticle = this.state.articleModification ? this.updateArticle : this.createArticle
 
         return (
             <div>
@@ -183,88 +230,98 @@ export class Homepage extends React.Component {
                     articlesCollection.map((article) => {
                         const myArticle = article.writerId === this.state.userId.toString()
                         const writer = myArticle ? 'moi' : article.writerName
-                        return(<>
-                            <Card key={article.Id}>
-                                <Card.Header>
-                                    Publié par{' '}
-                                    <b>{writer}{' '}</b>
-                                    <Moment
-                                        locale='fr'
-                                        calendar={calendarStrings}
-                                        date={article.timeStamp}
-                                    />
-                                </Card.Header>
-                                <Card.Body>
-                                    <Card.Title>{article.title}</Card.Title>
-                                    <Card.Img variant='top' src={article.imageUrl} alt='' />
-                                    <Card.Text>{article.text}</Card.Text>
-                                    <hr />
-                                    <Button onClick={() => this.articleModalDisplay(article)} >
-                                        <i className='fas fa-ellipsis-h'/>
-                                    </Button>
-                                </Card.Body>
-                                <Card.Footer>
-                                    <div className='thumbs'>
-                                        <i className='far fa-thumbs-up up'/>{article.likes}{' '}
-                                        <i className='far fa-thumbs-down down'/>{article.dislikes}{' '}
-                                         - score : {article.likes - article.dislikes}
-                                    </div>
-                                    {myArticle && (
-                                        <div className='card-footer-buttons'>
-                                            <Button onClick={() => this.modify(article)} ><i className='fas fa-edit'/></Button>
-                                            <Button onClick={() => this.deleteArticle(article)} ><i className='fas fa-trash-alt'/></Button>
-                                        </div>
-                                    )}
-                                </Card.Footer>
-                            </Card>
+                        const likeOption = (
+                            article.usersLiked.includes(this.state.userId) ? 1 
+                            : article.usersDisliked.includes(this.state.userId) ? 2
+                            : 3
+                        )
+                        const thumbUp = likeOption === 1 ? <i className="fas fa-thumbs-up"/> : <i className='far fa-thumbs-up'/>
+                        const thumbDown = likeOption === 2 ? <i className="fas fa-thumbs-down"/> : <i className='far fa-thumbs-down'/>
 
-                            <Modal show={this.state.showArticleModal} onHide={this.articleModalClose} animation={false}>
-                                <Modal.Header closeButton>
-                                    <Modal.Title>{this.state.article.title}</Modal.Title>
-                                </Modal.Header>
-                                <Modal.Body>{this.state.article.text}</Modal.Body>
-                                <Modal.Footer>
-                                    
-                                </Modal.Footer>
-                            </Modal>
-
-                            <Modal show={this.state.showCreateModal} onHide={this.createModalClose} animation={false}>
-                                <Modal.Header closeButton>
-                                    <Modal.Title>{this.state.articleModification ? 'Modifiez votre article :' : 'Ecrivez un article :' }</Modal.Title>
-                                </Modal.Header>
-                                <Modal.Body >
-                                    <Form noValidate>
-                                        <Form.Group controlId='title'>
-                                            <Form.Label>Titre :</Form.Label>
-                                            <Form.Control
-                                                className='input'
-                                                type='text'
-                                                name='newArticleTitle'
-                                                value={this.state.newArticleTitle}
-                                                onChange={this.handleInputChange}
-                                                required
-                                            />
-                                        </Form.Group> 
+                        return(
+                            <div key={article.Id}>
+                                <Card >
+                                    <Card.Header>
+                                        Publié par{' '}
+                                        <b>{writer}{' '}</b>
+                                        <Moment
+                                            locale='fr'
+                                            calendar={calendarStrings}
+                                            date={article.timeStamp}
+                                        />
+                                    </Card.Header>
+                                    <Card.Body>
+                                        <Card.Title>{article.title}</Card.Title>
+                                        <Card.Img variant='top' src={article.imageUrl} alt='' />
+                                        <Card.Text>{article.text}</Card.Text>
                                         <hr />
-                                        <Form.Group controlId='text'>
-                                            <Form.Label>Texte :</Form.Label>
-                                            <Form.Control as='textarea'
-                                                className='input'
-                                                type='text'
-                                                name='newArticleText'
-                                                value={this.state.newArticleText}
-                                                onChange={this.handleInputChange}
-                                                required
-                                            />
-                                        </Form.Group> 
-                                    </Form>
-                                </Modal.Body>
-                                <Modal.Footer>
-                                    <Button onClick={publishArticle}>Publier</Button>
-                                    <Button onClick={this.createModalClose}>Fermer</Button>
-                                </Modal.Footer>
-                            </Modal>
-                        </>)
+                                        <Button onClick={() => this.articleModalDisplay(article)} >
+                                            <i className='fas fa-ellipsis-h'/>
+                                        </Button>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                        <div className='thumbs'>
+                                            <div onClick={() => this.handleThumbUpChange(article, likeOption)}>{thumbUp}{article.likes}{' '}</div>
+                                            <div onClick={() => this.handleThumbDownChange(article, likeOption)}>{thumbDown}{article.dislikes}{' '}</div>
+                                            <div><b>Score : {article.likes - article.dislikes}</b></div>
+                                        </div>
+                                        {myArticle && (
+                                            <div className='card-footer-buttons'>
+                                                <Button onClick={() => this.modifyArticle(article)} ><i className='fas fa-edit'/></Button>
+                                                <Button onClick={() => this.deleteArticle(article)} ><i className='fas fa-trash-alt'/></Button>
+                                            </div>
+                                        )}
+                                    </Card.Footer>
+                                </Card>
+
+                                <Modal show={this.state.showArticleModal} onHide={this.articleModalClose} animation={false}>
+                                    <Modal.Header closeButton>
+                                        <Modal.Title>{this.state.article.title}</Modal.Title>
+                                    </Modal.Header>
+                                    <Modal.Body>{this.state.article.text}</Modal.Body>
+                                    <Modal.Footer>
+                                        
+                                    </Modal.Footer>
+                                </Modal>
+
+                                <Modal show={this.state.showCreateModal} onHide={this.createModalClose} animation={false}>
+                                    <Modal.Header closeButton>
+                                        <Modal.Title>{this.state.articleModification ? 'Modifiez votre article :' : 'Ecrivez un article :' }</Modal.Title>
+                                    </Modal.Header>
+                                    <Modal.Body >
+                                        <Form noValidate>
+                                            <Form.Group controlId='title'>
+                                                <Form.Label>Titre :</Form.Label>
+                                                <Form.Control
+                                                    className='input'
+                                                    type='text'
+                                                    name='newArticleTitle'
+                                                    value={this.state.newArticleTitle}
+                                                    onChange={this.handleInputChange}
+                                                    required
+                                                />
+                                            </Form.Group> 
+                                            <hr />
+                                            <Form.Group controlId='text'>
+                                                <Form.Label>Texte :</Form.Label>
+                                                <Form.Control as='textarea'
+                                                    className='input'
+                                                    type='text'
+                                                    name='newArticleText'
+                                                    value={this.state.newArticleText}
+                                                    onChange={this.handleInputChange}
+                                                    required
+                                                />
+                                            </Form.Group> 
+                                        </Form>
+                                    </Modal.Body>
+                                    <Modal.Footer>
+                                        <Button onClick={publishArticle}>Publier</Button>
+                                        <Button onClick={this.createModalClose}>Fermer</Button>
+                                    </Modal.Footer>
+                                </Modal>
+                            </div>
+                        )
                     })
                 }</main>
             </div>
